04cdffd3cf032beb83af6ca305b89b26b1b20654
testerlibo <jsjlibo@yeah.net>
2026-04-20 21:10:59 +0800

    删除本地测试文件

--- AirtestProject/test.air/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from airtest.core.api import *
-
-# 连接设备
-auto_setup(__file__)
-device_sn = "bbafe05b0604"
-connect_device(f"Android://127.0.0.1:5037/{device_sn}")
-print("✅ 设备已连接")
-
